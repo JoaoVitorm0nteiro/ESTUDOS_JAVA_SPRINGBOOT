397711e45906c991d808f5da6a86710a616d89a9
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-03-08 09:57:45 -0300

    revisando sintaxe da estrutura switchcase

---
+++ switchcase/src/switchcase.java
@@ -0,0 +1,14 @@
+public class switchcase {
+    public static void main(String[] args) throws Exception {
+        String sex = "F";
+        switch(sex){
+            case "F":
+                System.out.println("Female");
+                break;
+            case "M":
+            System.out.println("");
+            break;
+        }
+        
+    }
+}
