8f5b4149962e34a620f069c1de938e4c7d1e16ac
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-03-08 10:20:59 -0300

    exercicio fixação, mostrando todos os numeros pares de 1 a 1000000

---
+++ exerciciofor/src/exerciciofor.java
@@ -0,0 +1,10 @@
+public class exerciciofor {
+    public static void main(String[] args) throws Exception {
+        //exibir todos os numeros pares de 1 a 1000000
+        for(int i = 0; i<=1000000; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
