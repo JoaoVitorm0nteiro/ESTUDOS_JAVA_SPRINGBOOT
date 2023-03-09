8c4f27d0acab1cf15ce068e82cb9388bc50bf2b1
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-03-09 09:30:44 -0300

    estrutura for each

---
+++ arrays/src/Arrays.java
@@ -0,0 +1,11 @@
+
+//praticando  foreach
+public class Arrays {
+    public static void main(String[] args) throws Exception {
+        int [] numeros = {1,2,3,4,5,6,7,8,9,10};
+        for(int i:numeros){
+            System.out.println(i);
+        }
+    }
+}
+ 
